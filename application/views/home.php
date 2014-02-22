8c79d5e710a7672e3970aef80ecaa951532e0d69
Robby <robertvasen@hotmail.com>
2014-02-22 15:31:03 -0500

    added fancy matt hsu stuff to allow urls to work

--- application/views/home.php
+++ application/views/home.php
@@ -105,7 +105,7 @@
     <!-- Form -->
     <div class="container">
         <div class="row mt centered">
-            <form class="form-inline" role="form" action="form_handler.php" method="post">
+            <form class="form-inline" role="form" action="<?php echo base_url('form_handler')?>" method="post">
                 <div class="form-group">
                     <label class="sr-only" for="exampleInputEmail2">Full Name</label>
                     <input type="text" class="form-control" id="exampleInputEmail2" name="name" placeholder="Enter name">
