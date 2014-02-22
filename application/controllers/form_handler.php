f6b581c01698311dab91610b2a00672d71dcb806
Robby <robertvasen@hotmail.com>
2014-02-22 13:27:48 -0500

    [new] form

---
+++ application/controllers/form_handler.php
@@ -0,0 +1,13 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Form_handler extends CI_Controller {
+	public function doWork()
+	{
+		require('wunderground.php')
+		$APIkey = '660819cf2ae43c6e'
+		$name = $this->input->post('name');
+		$location = $this->input->post('location');
+		$gender = $this->input->post('gender');
+		$mathTest = (float) $this->input->post('mathTest');
+
+	}
