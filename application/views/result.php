111791ed2eb631e39eabe61cd818ae7cbfea0fe0
Vincent Chen <vincentschen@yahoo.com>
2014-02-22 19:52:07 -0500

    [new] working overlay

--- application/views/result.php
+++ application/views/result.php
@@ -16,7 +16,7 @@
 
     <!-- Custom styles for this template -->
     <link href="/css/main2.css" rel="stylesheet">
-
+    <link href="/css/main.css" rel="stylesheet">
     <!-- Fonts from Google Fonts -->
     <link href='http://fonts.googleapis.com/css?family=Lato:300,400,900' rel='stylesheet' type='text/css'>
 
@@ -60,13 +60,13 @@
 <div class="container" style="position:relative">
     <div class="row">
         <div class="col-lg-6">
-                <img id="base" src="../assets/img/clothes/base_male_character.png" alt="FashionBlueprint">
-                <img id="hat" src="../assets/img/clothes/sombrero.png" alt="FashionBlueprint">
-                <img id="shirt" src="../assets/img/clothes/tank_top.png" alt="FashionBlueprint">
-                <img id="pants" src="../assets/img/clothes/pants.png" alt="FashionBlueprint">
+                <img id="base" src="../assets/img/clothes/<?php echo $gender;?>" alt="FashionBlueprint">
+                <img id="hat" src="../assets/img/clothes/<?php echo $hat;?>" alt="FashionBlueprint">
+                <img id="shirt" src="../assets/img/clothes/<?php echo $shirt;?>" alt="FashionBlueprint">
+                <img id="pants" src="../assets/img/clothes/<?php echo $pants;?>" alt="FashionBlueprint">
         </div>
         <div class="col-lg-6">
-            <h4>Your Final Unique Outfit!</h4>
+            <h4> Your Unique Final Outfit!</h4>
             <p>This is how you will be looking in a matter of minutes! Take your first steps towards this future...</p>
         </div>
         </div>
