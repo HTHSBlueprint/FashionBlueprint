3663a69f2c354fc916d84aaaf0cf925b3ba05d9a
Yash Patel <yashpatel5400@gmail.com>
2014-03-26 17:27:20 -0400

    [new] Added button to home page and help page

---
+++ application/views/help.php
@@ -0,0 +1,82 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="assets/img/favicon.png">
+    <title>Fashionism - Change Your View of the World</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="/css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="/css/main.css" rel="stylesheet">
+
+    <!-- Fonts from Google Fonts -->
+    <link href='http://fonts.googleapis.com/css?family=Lato:300,400,900' rel='stylesheet' type='text/css'>
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+
+<!-- Analytics Tracking -->
+<?php include_once("analyticstracking.php") ?>
+
+
+<div class="navbar navbar-default navbar-fixed-top">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#"><b>Fashion Blueprint</b></a>
+        </div>
+        <div class="navbar-collapse collapse">
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#form">Already a fashionista?</a></li>
+            </ul>
+        </div><!--/.nav-collapse -->
+    </div>
+</div>
+
+<!-- Search Bar -->
+<div id="headerwrap">
+    <div class="container">
+        <div class="row">
+
+            <div class="col-lg-4">
+                <img class="img-responsive" src="assets/img/logo1.png" alt="FashionBlueprint" height="75%" width="75%">
+            </div>
+            <div class="col-md8">
+                <h1><br/><br/><br/>Fashion Your Seatbelts...<br/>
+                    It's Time to Ready Up.</h1>
+
+            </div><!-- /col-lg-6 -->
+        </div>
+    </div><!-- /container -->
+</div><!-- /headerwrap -->
+
+<div class="container">
+    <p class="centered">Created by Blueprint Team</p>
+</div><!-- /container -->
+
+
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js">
+    <script src="assets/js/bootstrap-theme.min.js"></script>
+<script src="js/bootstrap.js"></script>
+</body>
+</html>
